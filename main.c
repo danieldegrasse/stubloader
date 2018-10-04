ab6a81598d6f55a0b2ebe1518075e66ce1cc8ed4
Daniel DeGrasse <danieldegrasse@Daniels-HP-Spectre.localdomain>
2018-10-04 13:59:46 -0500

    Updated to use gnu-efi functions, now works correctly. May still have memory management issues

--- main.c
+++ main.c
@@ -1,26 +1,52 @@
+/*
+* main.c - a basic chainloader for linux EFI stub kernels
+* Copyright (C) 2018 Daniel DeGrasse <daniel@degrasse.com>
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <efi.h>
 #include <efilib.h>
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     InitializeLib(ImageHandle, SystemTable);
-    Print(L"Looking to see what our own loaded image handle looks like\n");
-    EFI_GUID loadedimageprotocol = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+    CHAR16 *efi_file = L"\\vmlinuz-linux";
+    CHAR16 *load_options=L"initrd=/initramfs-linux.img initrd=/intel-ucode.img cryptdevice=UUID=bd595533-df38-49c6-bac7-76d4b496fb98:cryptroot root=UUID=eb7a10ab-6501-4ebd-9978-2dc3f6195abc rw rootflags=rw,relatime quiet loglevel=3";
+    EFI_GUID gloadedimageprotocol = EFI_LOADED_IMAGE_PROTOCOL_GUID;
     EFI_LOADED_IMAGE_PROTOCOL *loadedImage;
     //query the handle to make sure the image protocol is supported
     //this will get use a pointer to the loaded image protocol
     EFI_BOOT_SERVICES *bootservices = SystemTable->BootServices;
     EFI_STATUS status = uefi_call_wrapper(bootservices->HandleProtocol,3,
                                                     ImageHandle,
-                                                    &loadedimageprotocol,
+                                                    &gloadedimageprotocol,
                                                     (void**)&loadedImage);
-    if (status != EFI_SUCCESS){
-        Print(L"Failed to get loaded image handle protocol\n");
-        Print(L"Status was %d\n",status);
-        return status;
+    switch(status){
+        case EFI_SUCCESS:
+            //Print(L"Loaded image protocol!\n");
+            break;
+        case EFI_UNSUPPORTED:
+            Print(L"Handle does not support protocol\n");
+            return status;
+        case EFI_INVALID_PARAMETER:
+            Print(L"Invalid Parameter\n");
+            return status;
     }
-    EFI_DEVICE_PATH_PROTOCOL *filepath = loadedImage->FilePath;
-    //here we also get the efi handle this file resides on, because that part of the handle is revelent as well
+
+    //now we will convert the handle this image is loaded on into a
+    //device path, and use that to make a new device path
+
     EFI_HANDLE esphandle = loadedImage->DeviceHandle;
     //now we need to get a device path for this handle
     EFI_GUID gdevicepathproto = EFI_DEVICE_PATH_PROTOCOL_GUID;
@@ -34,80 +60,22 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
             Print(L"did not find a device path for this handle\n");
             return status;
         case EFI_SUCCESS:
-            Print(L"got a device path for root\n");
+            //Print(L"got a device path for root\n");
             break;
     }
-    //try to open the efi device path to text protocol, to examine what is in this path
-    //this protocol is not installed on most handles, so we have to locate it.
-    EFI_DEVICE_PATH_TO_TEXT_PROTOCOL *pathtotextproto;
-    EFI_GUID pathtotextguid=EFI_DEVICE_PATH_TO_TEXT_PROTOCOL_GUID;
-    status = uefi_call_wrapper(bootservices->LocateProtocol,3,
-                                &pathtotextguid,
-                                NULL,//registration parameter
-                                (void**)&pathtotextproto);
-    if (status != EFI_SUCCESS){
-        Print(L"Error getting the path to text protocol\n");
-        Print(L"Status was %d\n",status);
-        return status;
-    }else if (pathtotextproto == NULL){
-        Print(L"No handle matched for the path to text proto\n");
-    }
-    //if we make it here, we have successfully found the protocol and we can use it.
-    CHAR16 *text_rep = (CHAR16*)uefi_call_wrapper(pathtotextproto->ConvertDevicePathToText,3,
-                    filepath,
-                    0,
-                    0);
-    if(text_rep != NULL){
-        Print(L"File Path of current executable: %s\n",text_rep);
-    }else{
-        Print(L"Failed to convert path\n");
-    }
-    text_rep = (CHAR16*)uefi_call_wrapper(pathtotextproto->ConvertDevicePathToText,3,
-                    rootpathproto,
-                    0,
-                    0);
-    if(text_rep != NULL){
-        Print(L"File Path of current executable: %s\n",text_rep);
-    }else{
-        Print(L"Failed to convert path\n");
-    }
-     
-    // now we will try to open the text to handle protocol and load an executable using it.
-    EFI_DEVICE_PATH_FROM_TEXT_PROTOCOL *pathfromtextproto;
-    EFI_GUID pathfromtextguid = EFI_DEVICE_PATH_FROM_TEXT_PROTOCOL_GUID;
-    status=uefi_call_wrapper(bootservices->LocateProtocol,3,
-                            &pathfromtextguid,
-                            NULL,//registration parameter, unused
-                            (void**)&pathfromtextproto);
-    if (status != EFI_SUCCESS){
-        Print(L"Failed to find path from text protocol! Status:%d \n",status);
-        return status;
-    }
-    //with the text, to handle protocol, we now want to try and open an efi 
-    //executable. Lets form a device handle for an executable at the root of our esp 
-    CHAR16 *fullpath =  L"PciRoot(0x0)/Pci(0x1,0x1)/Ata(Primary,Master,0x0)/HD(1,MBR,0xBE1AFDFA,0x3F,0xFBFC1)\\Print.efi";
-    Print(L"Locating %s\n",fullpath);
-    EFI_DEVICE_PATH_PROTOCOL *imgtoloadpath = (EFI_DEVICE_PATH_PROTOCOL*)uefi_call_wrapper(pathfromtextproto->ConvertTextToDevicePath,1,
-                                                            fullpath);
-    if(imgtoloadpath != NULL){
-        Print(L"Device Path Discovered!\n");
-    }else{
-        Print(L"Device Path Discovery Failed\n");
-        return EFI_NOT_FOUND;
-    }
-    //now try to load the image
-    EFI_HANDLE *imagetoloadhandle;
+    //now use the simple easy gnu efi function to make a full device path
+    EFI_DEVICE_PATH *imagetoloadpath = FileDevicePath(esphandle, efi_file);
+    EFI_HANDLE imgtoloadhandle;
     status = uefi_call_wrapper(bootservices->LoadImage,6,
-                                0, //we are the boot manager I guess
+                                FALSE,
                                 ImageHandle,
-                                imgtoloadpath,
+                                imagetoloadpath,
                                 NULL,
                                 0,
-                                &imagetoloadhandle);
-    Print(L"made it past load image\n");
+                                &imgtoloadhandle);
     switch(status){
         case EFI_SUCCESS:
-            Print(L"Image found and loaded!\n");
+            //Print(L"Image found and loaded!\n");
             break;
         case EFI_NOT_FOUND:
             Print(L"Image was not able to be located\n");
@@ -124,17 +92,53 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
         default:
             Print(L"Unhandled error, status %d\n",status);
             return status;
-        }
+    }
+    //now we need to add boot arguments to our image
+    EFI_LOADED_IMAGE_PROTOCOL *loadedimageproto;
+    status = uefi_call_wrapper(bootservices->HandleProtocol,3,
+                                imgtoloadhandle,
+                                &gloadedimageprotocol,
+                                (void**)&loadedimageproto);
+    switch(status){
+        case EFI_SUCCESS:
+            //Print(L"Got the loaded image protocol\n");
+            break;
+        case EFI_UNSUPPORTED:
+            Print(L"Device does not support this protocol\n");
+            return status;
+    }
+    //now change the image to have an argument
+    loadedimageproto->LoadOptions = load_options;
+    UINTN stringlen = StrSize(load_options);
+    loadedimageproto->LoadOptionsSize = stringlen;
+    
+
+    //now we have a loaded image, hopefully.
+    //not printing anything for max speed
+    //Print(L"Loading Next EFI Image\n");
+    status = uefi_call_wrapper(bootservices->StartImage,3,
+                                imgtoloadhandle,
+                                0,
+                                NULL);
+    switch(status){
+        case EFI_INVALID_PARAMETER:
+            Print(L"The image you want to start does not appear to be loaded\n");
+            return status;
+        default:
+            Print(L"Image did load and exit successfully\n");
+            return status;
+    }
+    //here we need to free any allocated memory we are responsible for
+    //should just be device path
+    status = uefi_call_wrapper(bootservices->FreePool,1,(void*)imagetoloadpath);
+    switch(status){
+        case EFI_SUCCESS:
+            Print(L"Memory Deallocated successfully\n");
+            break;
+        case EFI_INVALID_PARAMETER:
+            Print(L"Buffer to dealloc was invalid\n");
+            return status;
+    }
     return EFI_SUCCESS;
 }
-
-
-
-
-
-
-
-
-
-
 
